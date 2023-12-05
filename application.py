63118e0099bfb14969d55dd130569ce22d07cdc6
oksana_erm <oks.erm.012@gmail.com>
2023-12-05 03:40:49 +0000

    Update cors

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ socketio = SocketIO(application,
                     async_mode='gevent', 
                     engineio_logger=True, 
                     websocket_transports=['websocket', 'xhr-polling'],
-                    cors_allowed_origins=['http://itjinsights-env.eba-6cv9ca2d.eu-central-1.elasticbeanstalk.com/', 'localhost:8000']
+                    cors_allowed_origins='*'
                     )
 
 # celery
